d6d16db98d354cf48e053f1ae925e1d22ab7f7b6
Angela Samways <angelasamways@gmail.com>
2023-03-08 20:31:50 -0300

    Req9 fix

--- src/services/categoryService.js
+++ src/services/categoryService.js
@@ -1,11 +1,6 @@
 const { Category } = require('../models');
 
 const createCategory = ({ name }) => Category.create({ name });
-
-// const getByUserEmail = (email) => User.findOne({ where: { email } });
-
-// const getByUserId = (id) => User.findByPk(id, { attributes: { exclude: ['password'] } });
-
 const getCategories = () => Category.findAll();
 
 module.exports = {
